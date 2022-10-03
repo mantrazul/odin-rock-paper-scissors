9a7bcf884bb4e4a8060ae681fd29a2f4a76f4226
mantrazul <aspirina495@gmail.com>
2022-10-03 18:46:00 -0300

    Add score tally

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let userScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     const randomMove = Math.floor(Math.random()*3);
     switch (randomMove) {
@@ -18,34 +21,51 @@ function getComputerChoice() {
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             return "You lose.";
+            computerScore++;
         } else {
             return "You win";
+            userScore++;
         }
     }
 
     if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             return "You win";
+            userScore++;
         } else {
             return "You lose";
+            computerScore++;
         }
     }
 
     if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             return "You lose";
+            computerScore++;
         } else {
             return "You win";
+            userScore++;
         }
     }
  }
+ 
 function game() {
     playerSelection = prompt('Insert').toLowerCase();
     computerSelection = getComputerChoice();
     return console.log(playRound(playerSelection,computerSelection)) 
 }
 
+function checkWinner() {
+    if (computerScore > userScore) {
+        return console.log("Computer won!");
+    } else {
+        return console.log("Congratulations, you won!");
+    }
+}
+
 for ( i = 0; i < 5; i++) {
     game();
-    console.log(playerSelection,computerSelection);   
+    console.log(playerSelection,computerSelection);
 }
+
+checkWinner()
